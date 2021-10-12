951abb6c0098b4047c5abd7ae3f5ce08d22ae4e5
Bruno Ripa <bruno.ripa@gmail.com>
2021-10-12 23:46:10 +0100

    Wip

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,16 @@
 const SAY_URL = "https://api-gw-60qd84u6.nw.gateway.dev" + "/say";
 
-const ask = (question, custmerId) => {
+const ask = (question, customerId) => {
     return new Promise(async (resolve, reject) => {
         fetch(SAY_URL, {
             method: 'POST', body: JSON.stringify({ question: question }),
-            headers: { 'Agent': 'Butler v0.1', 'User-agent': 'Butler v0.1', 'Accept': '*/*', 'Content-Type': 'application/json' }
+            headers: {
+                'Agent': 'Butler v0.1',
+                'User-agent': 'Butler v0.1',
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+                'X-Customer-ID': customerId
+            }
         })
             .then(res => {
                 if (res.ok) {
